83f89661437846a2a7c8ab98dea51d070f860c9b
avyjyo11 <avy.jyo11@gmail.com>
2019-11-26 12:41:40 +0545

    ex6-normaization done

---
+++ javascript classtask/exercise6-normalization.js
@@ -0,0 +1,93 @@
+var input = {
+     '1': {
+          id: 1,
+          name: 'John',
+          children: [{
+                    id: 2,
+                    name: 'Sally'
+               },
+               {
+                    id: 3,
+                    name: 'Mark',
+                    children: [{
+                         id: 4,
+                         name: 'Harry'
+                    }]
+               }
+          ]
+     },
+     '5': {
+          id: 5,
+          name: 'Mike',
+          children: [{
+               id: 6,
+               name: 'Peter'
+          }]
+     }
+};
+
+var output = {};
+
+function solution(ip, op) {
+     for (key in ip) {
+          var obj = ip[key];
+          //console.log(obj);
+          recursion(obj, op);
+     }
+}
+
+function recursion(obj, op) {
+     var id2 = obj.id;
+     op[id2] = {
+          id: obj.id,
+          name: obj.name
+     }
+
+     if (obj.children) {
+          op[id2].children = obj.children.map(function (value) {
+               return value.id;
+          });
+          obj.children.map(function (value) {
+               for (key in value) {
+                    if (value.id) {
+                         recursion(value, op);
+                    }
+               }
+          });
+     } else {
+          //nothing
+     }
+}
+
+solution(input, output);
+console.log(output);
+// To this
+// var output = {
+//      '1': {
+//           id: 1,
+//           name: 'John',
+//           children: [2, 3]
+//      },
+//      '2': {
+//           id: 2,
+//           name: 'Sally'
+//      },
+//      '3': {
+//           id: 3,
+//           name: 'Mark',
+//           children: [4]
+//      },
+//      '4': {
+//           id: 4,
+//           name: 'Harry'
+//      },
+//      '5': {
+//           id: 5,
+//           name: 'Mike',
+//           children: [6]
+//      },
+//      '6': {
+//           id: 6,
+//           name: 'Peter'
+//      }
+// };
