f1c0171a67c4b8d510bdcc80f317cf65cfa42b74
avyjyo11 <avy.jyo11@gmail.com>
2019-11-28 08:44:49 +0545

    changes in js-assingment1 and 2

--- js-assignment-1-image-slides/script.js
+++ js-assignment-1-image-slides/script.js
@@ -1,21 +1,23 @@
 var carouselContainer = (document.getElementsByClassName('carousel-container'))[0];
 var carouselImageWrapper = (document.getElementsByClassName('carousel-image-wrapper'))[0];
-var imgList = document.getElementsByTagName('img');
+var imgList = carouselImageWrapper.children;
 var prevBtn = document.getElementById('prevBtn');
 var nextBtn = document.getElementById('nextBtn');
 var indicatorsDiv = document.getElementById('indicators');
 
 var containerWidth = carouselContainer.offsetWidth;
+console.log(containerWidth);
 var containerHeight = carouselContainer.offsetHeight;
 var numberOfImages = imgList.length;
-var holdTime = 1;
-var transitionTime = 2;
+var transitionTime = 10;
+var holdTime = 2;
 var counter = 0;
-var currentPos = counter * containerWidth;
-var nextPos = currentPos + containerWidth;
-var LAST_POS = (containerWidth * (imgList.length - 2));
-var INITIAL_POS = 0;
-var transitionInterval = (transitionTime * 1000) / containerWidth;
+var LAST_POS = containerWidth * numberOfImages;
+console.log(LAST_POS);
+var FIRST_POS = 0;
+var leftPos = 0;
+var rightPos = containerWidth;
+var autoSlide;
 
 carouselImageWrapper.style.width = (containerWidth * imgList.length) + 'px';
 carouselImageWrapper.style.height = containerHeight + 'px';
@@ -25,8 +27,6 @@ carouselImageWrapper.style.top = 0 + 'px';
 for (var i = 0; i < numberOfImages; i++) {
   imgList[i].style.width = containerWidth + 'px';
   imgList[i].style.height = containerHeight + 'px';
-  imgList[i].style.top = 0 + 'px';
-  imgList[i].style.left = (containerWidth * i) + 'px';
 }
 
 for (var i = 0; i < numberOfImages; i++) {
@@ -48,118 +48,86 @@ var indicators = indicatorsDiv.children;
 indicators[0].style.backgroundColor = 'rgba(255,255,255,1)';
 indicators[0].style.width = '18px';
 indicators[0].style.height = '18px';
-prevBtn.style.display = 'none';
 
-var autoSlideRight = setInterval(autoSlidingRight, (holdTime * 1000 + transitionTime * 1000));
+nextBtn.addEventListener('click', rightSlide);
+prevBtn.addEventListener('click', leftSlide);
+indicatorsDiv.addEventListener('click', moveToIndicated);
 
-function autoSlidingRight() {
+autoSlide = setInterval(rightSlide, (holdTime * 1000 + transitionTime));
 
-  if (currentPos >= LAST_POS) {
-    //console.log('cleared: ', carouselImageWrapper.style.left, currentPos);
-    var posL = -(currentPos + containerWidth);
-    var autoSlideLeft = setInterval(slidingLeft, transitionTime);
+function rightSlide() {
+  var pos = 0;
+  clearInterval(autoSlide);
+  var int1 = setInterval(rightFrame, transitionTime);
+  autoSlide = setInterval(rightSlide, (transitionTime + holdTime * 1000));
 
-    function slidingLeft() {
-      if (posL >= INITIAL_POS) {
-        clearInterval(autoSlideLeft);
-        //console.log(posL);
+  function rightFrame() {
+    if (counter == (numberOfImages - 1)) {
+      if (pos >= (LAST_POS - containerWidth)) {
+        rightPos = containerWidth;
+        leftPos = 0;
         counter = 0;
-        currentPos = 0;
-        nextPos = containerWidth;
-        prevBtn.style.display = 'none';
-        nextBtn.style.display = 'block';
-        //console.log(counter);
-        for (i = 0; i < numberOfImages; i++) {
-          if (i == counter) {
-            indicators[i].style.backgroundColor = 'rgba(255,255,255,1)';
-            indicators[i].style.width = '18px';
-            indicators[i].style.height = '18px';
-          } else {
-            indicators[i].style.backgroundColor = 'rgba(255,255,255,0.5)';
-            indicators[i].style.width = '16px';
-            indicators[i].style.height = '16px';
-          }
-        }
-
+        carouselImageWrapper.style.left = 0 + 'px';
+        clearInterval(int1);
+        setters();
       } else {
-        posL = posL + (transitionInterval * 5);
-        carouselImageWrapper.style.left = posL + 'px';
+        pos = pos + (LAST_POS - containerWidth) * 0.025;
+        carouselImageWrapper.style.left = (-(leftPos) + pos) + 'px';
+      }
+    } else {
+      if (leftPos >= rightPos) {
+        rightPos = rightPos + containerWidth;
+        leftPos = rightPos - containerWidth;
+        (counter) ++;
+        carouselImageWrapper.style.left = -(leftPos) + 'px';
+        clearInterval(int1);
+        setters();
+      } else {
+        leftPos = leftPos + containerWidth * 0.025;
+        carouselImageWrapper.style.left = -(leftPos) + 'px'
       }
     }
-  } else {
-    var pos = 0;
-    var slideRight = setInterval(slidingRight, transitionTime);
-    //console.log(counter);
+  }
 
-    function slidingRight() {
-      if (pos >= containerWidth) {
-        clearInterval(slideRight);
-        counter++;
-        setter();
+} //rightSlide
 
+function leftSlide() {
+  var pos = 0;
+  clearInterval(autoSlide);
+  var int2 = setInterval(leftFrame, transitionTime);
+  autoSlide = setInterval(rightSlide, (transitionTime + holdTime * 1000));
+
+  function leftFrame() {
+    if (counter == 0) {
+      if (pos >= (LAST_POS - containerWidth)) {
+        rightPos = LAST_POS;
+        leftPos = LAST_POS - containerWidth;
+        counter = (numberOfImages - 1);
+        carouselImageWrapper.style.left = leftPos + 'px';
+        clearInterval(int2);
+        setters();
       } else {
-        pos = pos + transitionInterval;
-        carouselImageWrapper.style.left = (-currentPos - pos) + 'px';
+        pos = pos + (LAST_POS - containerWidth) * 0.025;
+        carouselImageWrapper.style.left = (-(leftPos) - pos) + 'px';
+      }
+    } else {
+      if (rightPos <= leftPos) {
+        leftPos = leftPos - containerWidth;
+        rightPos = leftPos + containerWidth;
+        counter--;
+        carouselImageWrapper.style.left = -(rightPos - containerWidth) + 'px';
+        clearInterval(int2);
+        setters();
+      } else {
+        rightPos = rightPos - containerWidth * 0.025;
+        carouselImageWrapper.style.left = -(rightPos - containerWidth) + 'px';
       }
     }
-
-    currentPos = (containerWidth * counter);
-    nextPos = currentPos + containerWidth;
-    //console.log(carouselImageWrapper.style.left);
-  }
-}
-
-
-
-prevBtn.addEventListener('click', function (e) {
-  var pos = 0;
-  var prevBtnSlide = setInterval(frameLeft, transitionTime);
-
-  function frameLeft() {
-    if (pos >= containerWidth) {
-      counter--;
-      currentPos = (containerWidth * counter);
-      nextPos = currentPos + containerWidth;
-      clearInterval(prevBtnSlide);
-      setter();
-    } else {
-      pos = pos + transitionInterval * 3;
-      carouselImageWrapper.style.left = (-nextPos + pos) + 'px';
-    }
-  }
-});
-
-nextBtn.addEventListener('click', function (e) {
-  var pos = 0;
-  var nextBtnSlide = setInterval(frameRight, transitionTime);
-
-  function frameRight() {
-    if (pos >= containerWidth) {
-      counter++;
-      currentPos = (containerWidth * counter);
-      nextPos = currentPos + containerWidth;
-      clearInterval(nextBtnSlide);
-      setter();
-
-    } else {
-      pos = pos + transitionInterval * 3;
-      carouselImageWrapper.style.left = (-nextPos - pos) + 'px';
-    }
-  }
-});
-
-function setter() {
-  if (counter == numberOfImages - 1) {
-    nextBtn.style.display = 'none';
-    prevBtn.style.display = 'block';
-  } else if (counter == 0) {
-    nextBtn.style.display = 'block';
-    prevBtn.style.display = 'none';
-  } else {
-    nextBtn.style.display = 'block';
-    prevBtn.style.display = 'block';
   }
 
+} //leftSlide
+
+function setters() {
   for (i = 0; i < numberOfImages; i++) {
     if (i == counter) {
       indicators[i].style.backgroundColor = 'rgba(255,255,255,1)';
@@ -173,12 +141,16 @@ function setter() {
   }
 }
 
-for (var i = 0; i < numberOfImages; i++) {
-  indicators[i].addEventListener('click', function (e) {
-    console.log(e.target);
-    counter = i;
-    currentPos = counter * containerWidth;
-    nextPos = currentPos + containerWidth;
-    carouselImageWrapper.style.left = -nextpos + 'px';
-  })
+function moveToIndicated(e) {
+  clearInterval(autoSlide);
+  for (var i = 0; i < indicators.length; i++) {
+    if (indicators[i] == e.target) {
+      counter = i;
+      leftPos = counter * containerWidth;
+      rightPos = leftPos + containerWidth;
+      carouselImageWrapper.style.left = (-leftPos) + 'px';
+      setters();
+    }
+  }
+  autoSlide = setInterval(rightSlide, (transitionTime + holdTime * 1000));
 }
